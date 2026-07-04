1568941bf369904f941109d3cc029d5534969f98
Akash Pawar <akashpawar.official.acc@gmail.com>
2026-07-04 22:52:18 +0530

    Runtime : 0 ms | Beats : 100.00% | Memory : 9.30 MB

---
+++ 119_Pascal's_Triangle_II/119_Pascal's_Triangle_II.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> triangle;
+        for (int i = 0; i <= rowIndex; i++) {  
+            vector<int> row(i + 1, 1); 
+            for (int j = 1; j < i; j++) { 
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]; 
+            } 
+            triangle.push_back(row);
+        }
+        return triangle[rowIndex] ;
+    }
+};
+
+
+// Time Comlexity : O(N²)
+// Space Complexity : O(N²)
